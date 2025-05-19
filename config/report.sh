565eaf2c95ec0a031decac520f11a89729b7d174
Minseok Doo <89321156+tkakrdpahd@users.noreply.github.com>
2025-05-19 11:08:28 -0400

    frontend 일단 굴러감

--- config/report.sh
+++ config/report.sh
@@ -1,1 +1,8 @@
 #!/bin/bash
+# root context
+tree . -I "frontend|node_modules|pnpm-lock.yaml" --prune
+cloc . --exclude-dir=dist,.next,node_modules --not-match-f='(^|/)pnpm-lock\.yaml$'
+# frontend context
+cd frontend
+tree . -I ".next|node_modules|pnpm-lock.yaml" --prune
+cloc . --exclude-dir=dist,.next,node_modules --not-match-f='(^|/)pnpm-lock\.yaml$'
--- config/run.sh
+++ config/run.sh
@@ -1,1 +1,4 @@
 #!/bin/bash
+
+# docker compose
+docker compose -f docker/docker-compose.yml up -d --build
